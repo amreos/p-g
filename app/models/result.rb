a9239286e162e5aaac598a3ff79daf230ab35890
amre <amr.samier@yahoo.com>
2017-08-24 04:46:59 +0000

    am

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,21 +1,21 @@
-require 'csv'
-class Result < ActiveRecord::Base
-  belongs_to :machine
-    def self.assign_row(row)
-        line, b, time, real, min, mean, max = row
-        if row[3] =~ /SUMMARY/
-       @midr = row[2].slice(1,3)
-       @mach = Machine.where(:mid => @midr).first
-       @result = Result.create(midr: time, machine_id: @mach.id  )
-      else
-       line, b, time, real, min, mean, max = row
-       @result.update(line: line, min: min, exps: real, max: max, ideal: mean, time: time  ) 
-          end
-      end
-   def self.import(file)
-   CSV.foreach(file.path, encoding: 'iso-8859-1:utf-8', skip_blanks: true) do |row|
-  next if row.all?(&:nil?)
-  result = Result.assign_row(row)
-           end
-end
-end
+require 'csv'     
+class Result < ActiveRecord::Base     
+belongs_to :machine     
+def self.assign_row(row)    
+    line, b, time, real, min, mean, max = row    
+    if row[3] =~ /SUMMARY/      
+   @midr = row[2].slice(1,3)      
+   @mach = Machine.where(:mid => @midr).first      
+   @result = Result.create(midr: time, machine_id: @mach.id  )     
+  else      
+   line, b, time, real, min, mean, max = row      
+   @result.update( exps: real, max: max, ideal: mean, time: time  )     
+      end      
+  end     
+ def self.import(file)     
+ CSV.foreach(file.path, encoding: 'iso-8859-1:utf-8', skip_blanks: true) do |row|       
+next if row.all?(&:nil?)      
+result = Result.assign_row(row)     
+       end     
+  end     
+end     
